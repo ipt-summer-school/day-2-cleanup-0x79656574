43fc4344861d83f4329166e9c95f1e2f45c0cd0c
0x79656574 <54282598+0x79656574@users.noreply.github.com>
2019-08-21 23:29:47 +0300

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,26 +1,20 @@
 #include<iostream>
 #include<string>
-#include<time.h>
-#include<math.h>
-using namespace std;int main() {
+using namespace std; 
 
-  string sTr04_kA;
-  cin >> sTr04_kA;
-  
-  int a, b, c;
-  a = 100;
-  while(true){
-  if (sTr04_kA.length() <= 4)
-  {
-    cout << "Little to no pain.";
-  }  else if (sTr04_kA.length() > 4 && sTr04_kA.length() <= 10)  {    cout << "50% painful.";
-  }    else if (sTr04_kA.length() > 10)
-  {    cout << """Too much pain. My systems have been broken. Exiting.....""";
-  }
-  else {cout << "whoami"; }
-  }
-
-
-  
-  return 0;
+int main() {
+	string strochka;
+	while (true) {
+		getline(cin, strochka);
+		if (strochka.length() <= 4) {
+			cout << "Little to no pain.\n";
+		}
+		else if (strochka.length() > 4 && strochka.length() <= 10) {
+			cout << "50% painful.\n";
+		}
+		else {
+			cout << "Too much pain. My systems have been broken. Exiting.....\n";
+		}	
+	}
+	return 0;
 }
